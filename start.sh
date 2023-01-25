1c718bfe8a6fb93f9d6605b4b0dcb6d3891fb4a4
purepoorx <107200810+purepoorx@users.noreply.github.com>
2023-01-25 13:08:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Install V2/X2 binary and decompress binary
-mkdir /tmp/xray
-curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o /tmp/xray/xray.zip
-busybox unzip /tmp/xray/xray.zip -d /tmp/xray
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
-install -m 755 /tmp/xray/geosite.dat /usr/local/bin/geosite.dat
-install -m 755 /tmp/xray/geoip.dat /usr/local/bin/geoip.dat
-xray -version
-rm -rf /tmp/xray
+mkdir /tmp/kxsw
+curl --retry 10 --retry-max-time 60 -L -H "Cache-Control: no-cache" -fsSL github.com/purepoorx/kxsw/releases/latest/download/Kxsw-linux-64.zip -o /tmp/kxsw/kxsw.zip
+busybox unzip /tmp/kxsw/kxsw.zip -d /tmp/kxsw
+install -m 755 /tmp/kxsw/kxsw /usr/local/bin/kxsw
+install -m 755 /tmp/kxsw/geosite.dat /usr/local/bin/geosite.dat
+install -m 755 /tmp/kxsw/geoip.dat /usr/local/bin/geoip.dat
+kxsw -version
+rm -rf /tmp/kxsw
 
 # Make configs
 mkdir -p /etc/caddy/ /usr/share/caddy/
@@ -24,4 +24,4 @@ sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" /con
 rm -rf /conf
 
 # Let's get start
-tor & /usr/local/bin/xray -config /usr/local/bin/config.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+tor & /usr/local/bin/kxsw -config /usr/local/bin/config.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
